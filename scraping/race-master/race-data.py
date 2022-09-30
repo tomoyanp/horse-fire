69ca513efd328195718bbf9132bda82016ab8ba3
tomoyanp <tomoyanpy@gmail.com>
2022-10-01 01:24:19 +0900

    m

--- scraping/race-master/race-data.py
+++ scraping/race-master/race-data.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-raceData = open("race_date.csv", "w", encoding="utf_8_sig")
+raceData = open("race_data.csv", "w", encoding="utf_8_sig")
 raceData.write("race_id,goal_order, start_number, horse_name, horse_age, weight,jockey_id,single_odds,hourse_weight,weight_change\n")
 
 index = 0
@@ -26,28 +26,32 @@ def getOdds(url):
     replaced_url = url.replace("/race/", "/odds/").replace("result.html", "index.html").replace("rf=race_list", "rf=race_submenu")
     driver = getDriver()
     driver.get(replaced_url)
-    time.sleep(3)
+    # time.sleep(3)
     # table = driver.find_element_by_name("Ninki")
     isExist = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.ID, 'Ninki')))
     if isExist:
         pageSource = driver.page_source
-        print(pageSource)
-    # req = requests.get(replaced_url)
+        # print(pageSource)
+        # req = requests.get(replaced_url)
         result = BeautifulSoup(pageSource, "html.parser")
-    # print(result)
-    # write_file.write(result.text)
+        # print(result)
+        # write_file.write(result.text)
         table = result.find("table", {"id": "Ninki"})
         trs = table.findAll("tr")
         rtn_obj = {}
         for tr in trs:
+            print(tr)
             tds = tr.findAll("td")
             if (len(tds) > 0):
-                name = tds[4].find("a").text
-                prize_odds = tds[6].find("span").text
-                prize_odds_from = prize_odds.split("-")[0].strip()
-                prize_odds_to = prize_odds.split("-")[1].strip()
-                rtn_obj[name] = (prize_odds_from + prize_odds_to) / 2
-                return rtn_obj
+                try:
+                    name = tds[4].find("a").text
+                    prize_odds = tds[6].find("span").text
+                    prize_odds_from = prize_odds.split("-")[0].strip()
+                    prize_odds_to = prize_odds.split("-")[1].strip()
+                    rtn_obj[name] = (float(prize_odds_from) + float(prize_odds_to)) / 2
+                except Exception as e:
+                    print(e)
+        return rtn_obj
 
 
 for url in urlList:
@@ -63,6 +67,7 @@ for url in urlList:
     
     #レースIDはURLから取得
     odds_obj = getOdds(url)
+    print(odds_obj)
     race_id = url.replace('https://race.netkeiba.com/race/result.html?race_id=','').split('&')[0]
     for tr in trs:
         tds = tr.findAll("td")
@@ -78,8 +83,12 @@ for url in urlList:
                 single_odds = tds[10].text.strip()
                 hourse_weight = tds[14].text.split("(")[0].strip()
                 weight_change = tds[14].text.split("(")[1].strip().rstrip(")")
-                raceData.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (race_id,goal_order, start_number, horse_name, horse_age, weight,jockey_id,single_odds,hourse_weight,weight_change,odds_obj[horse_name]) )
-    time.sleep(5)
+
+                try:
+                    raceData.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (race_id,goal_order, start_number, horse_name, horse_age, weight,jockey_id,single_odds,hourse_weight,weight_change,odds_obj[horse_name]) )
+                except Exception as e:
+                    print(e)
+    # time.sleep(5)
 
 raceData.close()
 urlList.close()
