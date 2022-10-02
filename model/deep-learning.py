768d647ae0c2ca4347c2d9143067b4ea508d42fd
tomoyanp <tomoyanpy@gmail.com>
2022-10-02 15:17:50 +0900

    m

--- model/deep-learning.py
+++ model/deep-learning.py
@@ -1,3 +1,4 @@
+# coding: utf-8 
 from keras.optimizers import Adam
 import tensorflow as tf
 import numpy as np
@@ -11,42 +12,99 @@ M = 2  # 出力層の数
 
 
 data = pandas.read_csv("std_race_data.csv")
+
+# カラム指定の上列抽出
+# この時点では
+# rade_id, horse_age, weight...の行列
 training_data = data.loc[:, ["race_id", "horse_age",
-                             "weight", "hourse_weight", "weight_change"]]
+                             "weight", "hourse_weight", "weight_change", "single_odds", "multi_odds"]]
 np_array = training_data.values
 
+# mapにばらして、race_idごとに分けていく
+# つまり
+# mp[race_id_no1] = [
+#   [1,2,3,4,5,6],
+#   [1,2,3,4,5,6],
+# ]
+# みたいなデータが出来る
+# ついでに正解データも作る
 mp = {}
+mp_answer = {}
 for r in np_array:
     if r[0] in mp:
         mp[r[0]].append(r[1:5].tolist())
+        mp_answer[r[0]].append(r[5:7].tolist())
     else:
         mp[r[0]] = []
+        mp_answer[r[0]] = []
         mp[r[0]].append(r[1:5].tolist())
+        print(r)
+        mp_answer[r[0]].append(r[5:7].tolist())
 
-training_data = []
+tmp_data = []
+tmp_answer = []
 for key in mp.keys():
-    training_data.append(mp[key])
+    tmp_data.append(mp[key])
+    tmp_answer.append(mp_answer[key])
 
-# trainingList = np.array(training_data, dtype=float64)
-d = pandas.DataFrame(training_data)
-trainingList = d.values
+# ちょい怠いので一旦16頭建てのレース以外排除する
+training_data = []
+answer_data = []
+for index in range(0, len(tmp_data)):
+    if len(tmp_data[index]) == 16:
+        training_data.append(tmp_data[index])
+        answer_data.append(tmp_answer[index])
+
+trainingList = training_data
+answerList = answer_data
+# d = pandas.DataFrame(training_data)
+# a = pandas.DataFrame(answer_data)
+# trainingList = d.values
+# answerList = a.values
 
 # 入力データ
+
 # 学習データ(名前,騎手,会場など)
-# 教師データ(オッズ)
-# trainingList = np.array([[-0.3712348946887113, -0.18229082941864605, 0.4671866368196286, 1.0191835363608703],
-#                          [-0.3712348946887113, 0.8280348892713083, 1.6774307432691, -0.24170990967613754]])
+# trainingList = np.array([
+#     [
+#         [-0.6879920980515413, -0.6194127660107818, 0.1234, -0.1234],
+#         [-0.6358752736489328, -0.5028242358046408, 0.1234, -0.1234]
+#     ],
+#     [
+#         [-0.6879920980515413, -0.6194127660107818, 0.1234, -0.1234],
+#         [-0.6358752736489328, -0.5028242358046408, 0.1234, -0.1234]
+#     ]
+# ])
 
-testingList = np.array([[-0.6879920980515413, -0.6194127660107818],
-                       [-0.6358752736489328, -0.5028242358046408]])
+# 教師データ(単勝オッズ、複勝オッズ)
+# answerList = np.array([
+#     [
+#         [0.9, 0.3],
+#         [0.3, 0.1]
+#     ],
+#     [
+#         [0.9, 0.3],
+#         [0.3, 0.1]
+#     ]
+# ])
 
-testingList2 = np.array(
-    [[-0.3712348946887113, 0.8280348892713083, -0.27240698378838185, -0.24170990967613754]])
+# テストデータ
+# データ構造は学習データと同じ。学習済モデルの精度を上げるために必要
+# testingList = np.array([
+#     [
+#         [-0.6879920980515413, -0.6194127660107818, 0.1234, -0.1234],
+#         [-0.6358752736489328, -0.5028242358046408, 0.1234, -0.1234]
+#     ],
+#     [
+#         [-0.6879920980515413, -0.6194127660107818, 0.1234, -0.1234],
+#         [-0.6358752736489328, -0.5028242358046408, 0.1234, -0.1234]
+#     ]
+# ])
 
 
 # 3層ニューラルネットワークの構築
 model = tf.keras.Sequential()
-model.add(tf.keras.Input(shape=(N, 3)))
+model.add(tf.keras.Input(shape=(16, 4)))
 model.add(tf.keras.layers.Dense(K, activation='tanh'))
 model.add(tf.keras.layers.Dense(M, activation='softmax'))
 
@@ -60,12 +118,12 @@ model.compile(
 model.summary()
 
 # 固定回数（データセットの反復）の試行でモデルを学習
-model.fit(trainingList, testingList, None, 256)
+model.fit(trainingList, answerList, None, 256)
 
 # 出力 ※ 戻り値は numpy 配列
-y = model.predict(testingList2)
+# To Endo ここは任せた
+# y = model.predict(testingList)
 
-print('入力')
-tf.print(testingList)
-print('\n出力')
-print(y)
+
+
+
